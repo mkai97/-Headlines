37fd71b7bbfd2ed64b4d7976543353515ebfefcb
makai <1345744510@qq.com>
2019-08-20 21:24:44 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+<!--最外层容器-->
+  <el-container>
+<el-aside style='width:200px;height:100vh;background-color:#323745'>
+  <!--左侧-->
+   <layout-aside></layout-aside>
+</el-aside>
+ <el-container>
+  <el-header>头部</el-header>
+  <el-main>中间</el-main>
+   </el-container>
+   </el-container>
+</template>
+
+<script>
+import layoutAside from '../../componts/home/layout-aside'
+export default {
+  components: {
+    'layout-aside': layoutAside
+  }
+}
+</script>
