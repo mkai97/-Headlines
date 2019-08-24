fcc195432809bf86928b93b8e5afe4e088bae52c
makai <1345744510@qq.com>
2019-08-24 09:49:54 +0800

    素材管理页面构建

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,30 +1,166 @@
 <template>
-    <el-card>
-      <bread-crumb slot='header'>
-        <!-- 具名插槽 -->
-        <template slot='title'>素材管理</template>
-      </bread-crumb>
-      <!--el-tabs中 v-model属性绑定是当前子标签页的name -->
-       <el-tabs v-model="activeName" >
-        <el-tab-pane label="全部图片" name="first">
-
-        </el-tab-pane>
-        <el-tab-pane label="收藏图片" name="second">
-
-        </el-tab-pane>
-  </el-tabs>
+  <el-card>
+    <bread-crumb slot="header">
+      <!-- 具名插槽 -->
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-upload :show-file-list="false" action="" :http-request="uploadImg" class="upload-material">
+      <el-button type="primary">上传图片</el-button>
+    </el-upload>
+    <!--el-tabs中 v-model属性绑定是当前子标签页的name -->
+    <el-tabs @tab-click="changeTab" v-model="activeName">
+      <el-tab-pane label="全部图片" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt srcset />
+            <el-row align="middle" class="operate" type="flex" justify="space-around">
+              <i
+                :style="{color:item.is_collected? 'red':''}"
+                @click="collectOrCancel(item)"
+                class="el-icon-star-on"
+              ></i>
+              <i @click="delMaterial(item)" class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 <script>
 export default {
   data () {
     return {
-      activeName: 'first'
+      activeName: 'all',
+      list: [],
+      page: {
+        currentPage: 1,
+        pageSize: 15,
+        total: 0
+      }
+    }
+  },
+  methods: {
+    uploadImg (params) {
+      let formData = new FormData()
+      formData.append('image', params.file)
+      this.$axios({
+        method: 'post',
+        url: '/user/images',
+        data: formData
+      }).then(result => {
+        this.getMaterial()
+      })
+    },
+    delMaterial (item) {
+      this.$confirm('您确定删除此图片吗', '提示').then(() => {
+        this.$axios({
+          method: 'delete',
+          url: `/user/images/${item.id}`
+        }).then(result => {
+          this.getMaterial() // 重新获取数据
+        })
+      })
+    },
+    collectOrCancel (item) {
+      let mess = item.is_collected ? '取消收藏' : '收藏'
+      this.$confirm(`您是否要${mess}此图片?`, '提示').then(() => {
+        this.$axios({
+          method: 'put',
+          url: `/user/images/${item.id}`,
+          data: { collect: !item.is_collected }
+        }).then(result => {
+          this.getMaterial()
+        })
+      })
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
+    changeTab () {
+      this.page.currentPage = 1
+      this.getMaterial()
+    },
+    getMaterial () {
+      let PageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect',
+          ...PageParams
+        }
+      }).then(result => {
+        this.list = result.data.results
+        this.page.total = result.data.total_count
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
+  }
+}
+</script>
+<style lang="less">
+.upload-material {
+  position: absolute;
+  right:20px;
+  margin-top: -10px;
+}
+.img-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 200px;
+    margin: 20px 10px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 30px;
+      width: 100%;
+      background-color: #f4f5f6;
+      i {
+        font-size: 22px;
+      }
     }
   }
 }
-
-</script>
-<style lang="less">
-
 </style>
