e635717abb7778151fa60bb47a84886abd8b2bff
makai <1345744510@qq.com>
2019-08-24 11:59:41 +0800

    内容列表-页面结构-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -47,14 +47,18 @@
         </div>
         <div class="right">
           <span>
-            <i class="el-icon-edit"></i>
+            <i class="el-icon-edit">修改</i>
           </span>
           <span>
-            <i class="el-icon-delete"></i>
+            <i class="el-icon-delete">删除</i>
           </span>
         </div>
       </div>
     </div>
+    <el-row type="flex" justify="center" style="margin:20px 0 " >
+        <el-pagination @current-change="changePage" :current-page="page.currentPage" :page-size="page.pageSize" background layout="prev, pager, next" :total="page.total"></el-pagination>
+
+    </el-row>
   </el-card>
 </template>
 
@@ -75,25 +79,35 @@ export default {
     }
   },
   methods: {
-    refreshList () {
+    getConditions () {
       let { status, channel_id: cid, dateRange } = this.formData
       let params = {
         status: status === 5 ? null : status,
         channel_id: cid,
         begin_pubdate: dateRange && dateRange.length ? dateRange[0] : null,
-        end_pubdate: dateRange && dateRange.length > 1 ? dateRange[1] : null
-      }
-      this.getArticles(params)
+        end_pubdate: dateRange && dateRange.length > 1 ? dateRange[1] : null }
+      params.page = this.page.currentPage
+      params.per_page = this.page.pageSize
+      return params
+    },
+    refreshList () {
+      this.page.currentPage = 1
+      this.getArticles(this.getConditions()) // 调用查询接口 传入参数
     },
     getArticles (params) {
       this.$axios({
         url: '/articles',
         params: { ...params }
       }).then(result => {
-        this.list = result.data.results
-        this.page.total = result.data.total_count
+        this.list = result.data.results // 将当前的数据赋值给data中对象
+        this.page.total = result.data.total_count // 当前总条数
       })
     },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getArticles(this.getConditions())
+    },
+
     getChannels () {
       this.$axios({
         url: '/channels'
@@ -130,7 +144,7 @@ export default {
     }
   },
   created () {
-    this.getArticles()
+    this.getArticles({ page: 1, per_page: 10 })
     this.getChannels()
   }
 }
